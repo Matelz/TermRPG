f670d3f9d7ac752ea79ac0af6a8086927f71c7ca
Matt <mateuscaxambu@gmail.com>
2024-04-03 07:45:33 -0300

    feature: change hero's name

--- TestePersonagem.java
+++ TestePersonagem.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 public class TestePersonagem {
     public static void main(String[] args) {
         Random random = new Random();
-
         Boolean running = true;
-
         Personagem personagem = new Personagem();
-        personagem.nascer("Jonas", random.nextInt(11), random.nextInt(11), random.nextInt(11));
-
-        while (running){        
-            Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        
+        System.out.printf("Insira um nome para o seu herói:\n-> ");
+        String name = input.nextLine(); 
+        personagem.nascer(name, random.nextInt(11), random.nextInt(11), random.nextInt(11));
+        
+        while (running){ 
             var choice = "";
 
             System.out.printf("\nDigite o que você deseja fazer:\n\t📜 (S) - Status\n\t🏹 (C) - Caçar\n\t🏔️  (E) - Explorar\n\t🍔 (O) - Comer\n\t🛏️  (D) - Dormir\n\t❌ (X) - Sair\n");
